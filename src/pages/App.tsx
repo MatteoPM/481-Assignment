8a19f63bc09419fcc0779a24e3df4d359326ea29
Danny Duong <dannyduong4.00@outlook.com>
2024-11-08 14:20:05 -0700

    Dashboard

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,32 +1,38 @@
+import EventCard from "@/components/eventCard";
+import ForumCard from "@/components/forumCard";
+import GroupCard from "@/components/groupCard";
 import Page from "@/components/page";
-import Card from "../components/card";
+import { events } from "@/placeholderData";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Page title="Dashboard">
         <h2 className="text-xl font-semibold">Recent Posts</h2>
-        <div className="mt-1 flex gap-2 overflow-x-auto rounded-md py-2 scrollbar">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="mt-3 flex flex-col divide-y overflow-hidden rounded-md border bg-white shadow-sm">
+          <ForumCard />
+          <ForumCard />
+          <ForumCard />
+          <ForumCard />
         </div>
 
+        <Link to={""} className="py-2 text-sm text-muted-foreground">
+          View all
+        </Link>
+
         <h2 className="mt-8 text-xl font-semibold">Upcoming Events</h2>
-        <div className="mt-1 flex gap-2 overflow-x-auto rounded-md py-2">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="mt-6 space-y-3">
+          <EventCard event={events[0]} />
+          <EventCard event={events[1]} />
+          <EventCard event={events[2]} />
         </div>
 
         <h2 className="mt-8 text-xl font-semibold">Suggested Groups</h2>
-        <div className="mt-1 flex gap-2 overflow-x-auto rounded-md py-2">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="mt-3 flex flex-col divide-y overflow-hidden rounded-md border bg-white shadow-sm">
+          <GroupCard compact />
+          <GroupCard compact />
+          <GroupCard compact />
         </div>
       </Page>
     </>
