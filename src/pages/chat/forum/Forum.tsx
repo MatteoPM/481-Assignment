7743ed8692ed804309eac6ba13c697fcdf0e0943
Danny Duong <dannyduong4.00@outlook.com>
2024-11-16 13:41:08 -0700

    asdfgsgrthrth

--- src/pages/chat/forum/Forum.tsx
+++ src/pages/chat/forum/Forum.tsx
@@ -1,6 +1,8 @@
 import Page from "@/components/page";
+import SubHeader from "@/components/subHeader";
 import GroupCard from "@/pages/groups/_components/groupCard";
 import { forums, groups } from "@/placeholderData";
+import { Users } from "lucide-react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import ChatMessage from "../_components/chatMessage";
@@ -24,7 +26,9 @@ function Forum() {
     <>
       <Page title={forum.title} showBackButton hideFooter>
         <div className="flex h-full flex-col">
-          <div className="mb-4 border-b pb-4">
+          <SubHeader Icon={Users} text="Group" className="mt-0" />
+
+          <div className="mb-4 mt-2 border-b pb-4">
             <GroupCard group={group} />
           </div>
 
